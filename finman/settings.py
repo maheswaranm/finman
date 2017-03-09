fe72cd883f2def0ae3bad78ef1a03c7398b20af2
Maheswaran M <maheswaranmoothiringode@gmail.com>
2017-03-09 06:59:04 -0500

    add finmancore app to installed apps

--- finman/settings.py
+++ finman/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'finmancore.apps.FinmancoreConfig'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
