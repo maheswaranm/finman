b370d425a3a9fab70364b05fbb0537c24ee6a56f
Maheswaran M <maheswaranmoothiringode@gmail.com>
2017-03-09 07:01:17 -0500

    adding a comma for finmancore in installed apps

--- finman/settings.py
+++ finman/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'finmancore.apps.FinmancoreConfig'
+    'finmancore.apps.FinmancoreConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
