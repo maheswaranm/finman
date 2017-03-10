0ed01f07ebb1837e57956717e575b97bef6c4c8b
Maheswaran M <maheswaranmoothiringode@gmail.com>
2017-03-10 07:24:26 -0500

    basic header items - no style

--- finmancore/views.py
+++ finmancore/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
+from .models import Account
+
 # Create your views here.
 @login_required(login_url='/login')
 def home_view(request):
+    accounts = Account.objects.all()
+    context = {'accounts': accounts}
     return render(request,'home.html',context)
