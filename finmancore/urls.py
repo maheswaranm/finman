7f81d6e691d22adfebb84e6e5892e6c0897396a6
Maheswaran M <maheswaranmoothiringode@gmail.com>
2017-03-10 07:57:31 -0500

    dummy views for all header URLs

--- finmancore/urls.py
+++ finmancore/urls.py
@@ -3,5 +3,11 @@ from . import views
 
 urlpatterns = [
     url(r'^$',views.home_view),
-    url(r'^home',views.home_view)
+    url(r'^home',views.home_view),
+    url(r'^account/(?P<account_id>[0-9]+)',views.account_view),
+    url(r'^account/manage',views.account_manage),
+    url(r'^account/new',views.account_new),
+    url(r'^profile/changeuser',views.change_user),
+    url(r'^profile/changepass',views.change_pass),
+    url(r'^categories/manage',views.category_manage),
 ]
--- finmancore/views.py
+++ finmancore/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 
 from .models import Account
 
@@ -9,3 +10,27 @@ def home_view(request):
     accounts = Account.objects.all()
     context = {'accounts': accounts}
     return render(request,'home.html',context)
+
+@login_required(login_url='/login')
+def account_view(request,account_id):
+    return HttpResponse('account page for '+account_id)
+
+@login_required(login_url='/login')
+def account_manage(request):
+    return HttpResponse('account manage page')
+
+@login_required(login_url='/login')
+def account_new(request):
+    return HttpResponse('new account page')
+
+@login_required(login_url='/login')
+def change_user(request):
+    return HttpResponse('change user view')
+
+@login_required(login_url='/login')
+def change_pass(request):
+    return HttpResponse('change pass view')
+
+@login_required(login_url='/login')
+def category_manage(request):
+    return HttpResponse('category manage page')
