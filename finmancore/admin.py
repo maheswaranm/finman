fa03b2d8b63a73f09a1441955a03cfd0a841f198
Maheswaran M <maheswaranmoothiringode@gmail.com>
2017-03-09 19:56:35 -0500

    basic model for finmancore

--- finmancore/admin.py
+++ finmancore/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Account,Credit,Debit,Transfer,Transaction,Category
+
+admin.site.register(Account)
+admin.site.register(Category)
+admin.site.register(Credit)
+admin.site.register(Debit)
+admin.site.register(Transfer)
--- finmancore/models.py
+++ finmancore/models.py
@@ -1,3 +1,55 @@
 from django.db import models
+from model_utils.managers import InheritanceManager
 
 # Create your models here.
+class Account(models.Model):
+    label = models.CharField(max_length=100)
+    currency = models.CharField(max_length=3)
+    balance = models.DecimalField(max_digits=10,decimal_places=2,default=0)
+
+    def __str__(self):
+        return "<Account : "+self.label+">"
+
+class Category(models.Model):
+    label = models.CharField(max_length=50)
+    parent = models.ForeignKey("self",related_name="parent_category",default=None, blank=True, null=True)
+
+    class Meta:
+        verbose_name_plural = "categories"
+
+    def __str__(self):
+        return "<Category : "+self.label+">"
+
+class Transaction(models.Model):
+    amount = models.DecimalField(max_digits=10,decimal_places=2,default=0)
+    time_of_transaction = models.DateTimeField()
+    category = models.ForeignKey(Category,default=None, blank=True, null=True)
+    is_reconciled = models.BooleanField(default=False)
+    currency = models.CharField(max_length=3,default=None, blank=True, null=True)
+
+    #This is so I can find type when displaying on template
+    def type_of_transaction(self):
+        return self.__class__.__name__
+
+    objects = InheritanceManager()
+
+class Transfer(Transaction):
+    from_account = models.ForeignKey(Account,related_name='from_account')
+    to_account = models.ForeignKey(Account,related_name='to_account')
+
+    def __str__(self):
+        return "<Transfer :"+str(self.id)+" from "+str(self.from_account)+" to "+str(self.to_account)+">"
+
+class Credit(Transaction):
+    from_account = models.CharField(max_length=500)
+    to_account = models.ForeignKey(Account,related_name='credited_account')
+
+    def __str__(self):
+        return "<Credit "+str(self.id)+" from "+str(self.from_account)+" to "+str(self.to_account)+">"
+
+class Debit(Transaction):
+    from_account = models.ForeignKey(Account,related_name='debited_account')
+    to_account =  models.CharField(max_length=500)
+
+    def __str__(self):
+        return "<Debit "+str(self.id)+" from "+str(self.from_account)+" to "+str(self.to_account)+">"
