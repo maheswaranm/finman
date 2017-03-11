eb138796d8f5bbc4052e51e59a8056f31e37a5b2
Maheswaran M <maheswaranmoothiringode@gmail.com>
2017-03-11 13:05:35 -0500

    added django-debug-toolbar

--- finman/settings.py
+++ finman/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar'
 ]
 
 MIDDLEWARE = [
@@ -48,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'finman.urls'
@@ -114,6 +116,7 @@ USE_L10N = True
 
 USE_TZ = True
 
+INTERNAL_IPS = ['localhost','127.0.0.1']
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
