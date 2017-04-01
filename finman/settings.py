2480de5e2e21b4ae78cd163ee7c805a7ae5956f6
Maheswaran M <maheswaranmoothiringode@gmail.com>
2017-04-01 19:24:24 -0400

    added dj_database_url and used that for DB

--- finman/settings.py
+++ finman/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 
 import os
 
+import dj_database_url
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -77,11 +79,9 @@ WSGI_APPLICATION = 'finman.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
+db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': db_from_env
 }
 
 
@@ -123,7 +123,7 @@ INTERNAL_IPS = ['localhost','127.0.0.1']
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = '/home'
