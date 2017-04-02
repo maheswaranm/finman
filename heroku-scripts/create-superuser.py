4b1d4991a6eeeba89bf4ba11123d90867052d460
Maheswaran M <maheswaranmoothiringode@gmail.com>
2017-04-02 13:15:21 -0400

    superuser script fix + added script to app.json

--- heroku-scripts/create-superuser.py
+++ heroku-scripts/create-superuser.py
@@ -1,5 +1,3 @@
-from django.contrib.auth.models
-
-import User;
+from django.contrib.auth.models import User;
 
 User.objects.create_superuser(os.environ['ADMIN_USERNAME'], os.environ['ADMIN_EMAIL'], os.environ['ADMIN_PASSWORD'])
