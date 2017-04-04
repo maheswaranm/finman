a3b071795960ec243babb1988f5a159633afbd96
Maheswaran M <maheswaranmoothiringode@gmail.com>
2017-04-03 23:14:20 -0400

    added datepicker with flatpickr based on floppyforms tutorial

--- finmancore/forms.py
+++ finmancore/forms.py
@@ -1,34 +1,47 @@
-from django.forms import ModelForm
+import floppyforms.__future__ as forms
 from django.contrib.auth.models import User
 
 from finmancore.models import Account,Credit,Debit,Transfer,Category
 
-class AccountForm(ModelForm):
+
+class DatePicker(forms.DateTimeInput):
+    template_name = 'datepicker.html'
+
+class AccountForm(forms.ModelForm):
     class Meta:
         model = Account
         fields = ['label','currency']
 
-class CreditForm(ModelForm):
+class CreditForm(forms.ModelForm):
     class Meta:
         model = Credit
         exclude = []
+        widgets = {
+            'time_of_transaction':DatePicker
+        }
 
-class DebitForm(ModelForm):
+class DebitForm(forms.ModelForm):
     class Meta:
         model = Debit
         exclude = []
+        widgets = {
+            'time_of_transaction':DatePicker
+        }
 
-class TransferForm(ModelForm):
+class TransferForm(forms.ModelForm):
     class Meta:
         model = Transfer
         exclude = []
+        widgets = {
+            'time_of_transaction':DatePicker
+        }
 
-class CategoryForm(ModelForm):
+class CategoryForm(forms.ModelForm):
     class Meta:
         model = Category
         exclude = []
 
-class UserNameChangeForm(ModelForm):
+class UserNameChangeForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['username']
